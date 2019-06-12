4e0258642b3c62381969745b41bb32308cdada53
Hatazima <nkc19.zyou.hatazima@gmail.com>
2019-06-12 16:37:53 +0900

    タイトルを追加

---
+++ Assets/Hatazima/TitleButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+
+    public void TitleButtonDown()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    void Update ()
+    {
+		
+	}
+}
