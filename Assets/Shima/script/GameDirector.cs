a4c1c092396cdfd2de1ea7ce9c012bf467a5a510
shima20192265 <nkc31.kojiro.shima@gmail.com>
2019-06-12 19:42:16 +0900

    マス目作成。プラスマスとマイナスマスのスクリプトの作成。

---
+++ Assets/Shima/script/GameDirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace shima
+{
+    public class GameDirector : MonoBehaviour
+    {
+        GameObject text;
+        int koukando = 50;//好感度の初期値
+
+        // Use this for initialization
+        void Start()
+        {
+            this.text = GameObject.Find("koukando");//テキストの呼び出し
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            this.text.GetComponent<Text>().text =
+                "好感度" + koukando;//好感度表示
+        }
+        public void koukandoUp()
+        {
+            koukando += 10;
+        }
+        public void koukandoDown()
+        {
+            koukando -= 10;
+        }
+    }
+}
---
+++ Assets/Shima/script/minusmasuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace shima
+{
+
+    public class minusmasuController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            GameObject director = GameObject.Find("GameDirector");
+            director.GetComponent<GameDirector>().koukandoDown();
+        }
+    }
+}
